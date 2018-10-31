5f14404c65668452cd3c24512cf99e132ba6edb4
Dima Kurbatov <dskurbatov13@gmail.com>
2018-10-31 15:40:34 -0700

    update reducer.js with object spread operator

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,9 +4,15 @@ const { initialState } = require('./initial_state')
 const reducer = (state=initialState, action) => {
   switch(action.type){
     case SET_REQUIREMENTS:
-      return Object.assign({}, state, {'requirements_loaded': action.payload})
+      return {
+        ...state,
+        "requirements_loaded": action.payload
+      }
     case SET_RESUME:
-      return Object.assign({}, state, {'resume_loaded': action.payload})
+      return {
+        ...state,
+        'resume_loaded': action.payload
+      }
     default:
       return state
   }
